db1791cecc4a64b86dfdc5530b7ddaac9405dd86
atroopa <atroopa@gmail.com>
2024-05-30 13:57:13 +0330

    njihij

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,43 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-        <div class=" border-b ">
-            <div class=" flex justify-center items-center">
-              <RouterLink to="/" as="a" class=" pr-10">
-                <img src="https://picsum.photos/200?random=1" alt="Logo" class=" rounded-full w-12 " />
-              </RouterLink>
-              <!-- Our link container -->
-              <div class="flex text-2xl space-x-2 h-12 justify-center items-center">
-                <HeaderLink to="/begin" text="Begin"/>
-                <HeaderLink to="/sales" text="Sales"/>
-                <HeaderLink to="/solutions" text="Solutions" />
-                <HeaderLink to="/photos" text="Photos" />
-                <HeaderLink to="/about-us" text="About us"/>
-                <HeaderLink to="/contact" text="Contact" />
-              </div>
+    <header>
+        <!-- component -->
+        <nav class="bg-gray-200 shadow shadow-gray-300 w-100 px-8 md:px-auto">
+            <div
+                class="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
+                <!-- Logo -->
+                <div class="text-indigo-500 md:order-1">
+                    <!-- Heroicon - Chip Outline -->
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
+                    </svg>
+                </div>
+                <div class="text-gray-500 order-3 w-full md:w-auto md:order-2">
+                    <ul class="flex font-semibold justify-between">
+                        <!-- Active Link = text-indigo-500
+                Inactive Link = hover:text-indigo-500 -->
+                        <li class="md:px-4 md:py-2 text-gray-900 hover:text-indigo-500"><router-link to="/">Home</router-link></li>
+                        <li class="md:px-4 md:py-2 text-gray-900 hover:text-indigo-500"><router-link to="/users">Users</router-link></li>
+                        <li class="md:px-4 md:py-2 text-gray-900 hover:text-indigo-500"><router-link to="/posts">Posts</router-link></li>
+                    </ul>
+                </div>
+                <div class="order-2 md:order-3">
+                    <button
+                        class="px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-gray-50 rounded-xl flex items-center gap-2">
+                        <!-- Heroicons - Login Solid -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd"
+                                d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <span>Login</span>
+                    </button>
+                </div>
             </div>
-          </div>
+        </nav>
+    </header>
 </template>
 <script>
 
---
+++ src/pages/Home.vue
@@ -0,0 +1,12 @@
+<template lang="">
+    <div>
+        <p class="mb-3 text-gray-800 dark:text-gray-400 first-line:uppercase first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:text-gray-900 dark:first-letter:text-gray-100 first-letter:me-3 first-letter:float-start">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
+        <p class="text-gray-800 dark:text-gray-400">Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease, with a complete audit trail for every change.</p>
+    </div>
+</template>
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Home'
+}
+</script>
